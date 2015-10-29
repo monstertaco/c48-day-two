520943c835454ecdb7a1349c7f8a14e762ed43f2
monstertaco <anderson.ajmc@gmail.com>
2015-10-28 21:01:14 -0700

    my first commit

---
+++ javascript/example.js
@@ -0,0 +1,16 @@
+
+var someNumber = 12;
+var anotherNumber = someNumber + 4;
+var name = "charlie";
+var aLocation = "university of washington";
+var quote = "here is a quote in a quote 'this is the quote'"
+console.log(someNumber);
+console.log(anotherNumber);
+console.log(quote);
+
+var favoriteFoods = ["pizza", "ice cream", "burgers"];
+console.log(favoriteFoods);
+console.log(favoriteFoods[1]);
+
+var thirdFav = favoriteFoods[2];
+console.log(thirdFav)
